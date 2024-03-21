75e6380ed871bc8c1b0f1db88704f685fd4649fe
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-03-20 17:42:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_Practice_Scope_and_Switch_Adam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
